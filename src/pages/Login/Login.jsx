728e4a0d4e786c1bd515837582c3ee0c8b650624
Mickaël Anderson <123479860+RaMiAnderson@users.noreply.github.com>
2024-09-08 22:42:52 +0300

    Disposition Login finished

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,13 @@ import LoginCover from "../../assets/cover/bg.svg"
 import Icon from "../../assets/ico/iconSymptoScan.png"
 
 //component
-import { TextField } from '@mui/material'
+import { TextField, Button } from '@mui/material'
+
+//ico
+import GoogleIcon from '../../assets/ico/icons8-google.svg'
+import FacebookIcon from '../../assets/ico/icons8-facebook.svg'
+import SymptoIcon from '../../assets/ico/couverture.png'
+
 
 //style
 import "./Login.css"
@@ -12,14 +18,11 @@ let Login = ()=> {
   return (
     <>
       <div className='loginParent'>
-        <section className="cover">
+        <section className="cover"> 
+          <p>+</p>
           <img src={LoginCover}  alt='Cover'/>
         </section>
         <section className='sectionForm'>
-          {/* logo of Hospital */}
-          <div>
-
-          </div>
 
           <div className='salutation'>
             <h2>Welcome Back Doctor !</h2>
@@ -27,8 +30,28 @@ let Login = ()=> {
           </div>
 
           <div className='divForm'>
-
+            <form action="#">
+              <TextField 
+                id='outlined-required'
+                label="Email"
+                defaultValue="" className='textField' />
+              <TextField 
+                id='outlined-password-input'
+                label="Password"
+                type='password'
+                autoComplete='current-password'
+                className='textField' />
+              
+              <Button type='submit' className='btnSubmit' variant="Contained">Se Connecter</Button>
+            </form>
+            <a href=""><Button className='btnOtherLog' variant="Contained">Autre methode de connexion</Button></a>
+            
+            <div className='forgotPasswSection'>
+              <Button className='otherLogBtn' variant='outlined'><img src={GoogleIcon} alt="" />Continuer avec Google</Button>
+              <Button className='otherLogBtn' variant='outlined' ><img  src={FacebookIcon} alt="" />Continuer avec Facebook</Button>
+            </div>
           </div>
+
         </section>
       </div>
       
